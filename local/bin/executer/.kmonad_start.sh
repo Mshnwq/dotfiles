a5958a5179e93702586b2e6e863861293eba0cf2
mshnwq <mshnwq.com@gmail.com>
2025-01-06 14:30:00 +0300

    exc kmonad

--- local/bin/executer/.kmonad_start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pgrep -f config.kbd || { kmonad "$HOME/.config/kmonad/config.kbd" & }
-notify-send -u low "Kmonad status" "On"
--- local/bin/executer/.kmonad_stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-pkill -f config.kbd && notify-send -u low "Kmonad status" "Off"
