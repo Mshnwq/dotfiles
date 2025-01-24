8419586a8804b76b3bf1c24acc4b3fdd5c9d616d
mshnwq <mshnwq.com@gmail.com>
2025-01-24 10:30:00 +0300

    hm old nvim

--- config/home-manager/nvim.nix
+++ config/home-manager/nvim.nix
@@ -1,16 +1,17 @@
 { config, pkgs, lib, ... }: {
-  home.packages = [
-    pkgs.direnv
-    pkgs.neovim
-    pkgs.nodejs
-    pkgs.python313Packages.flake8
-    pkgs.python313Packages.isort
-    pkgs.python313Packages.watchdog
+  home.packages = with pkgs; [
+    direnv
+    # devenv
+    neovim
+    nodejs
+    python313Packages.flake8
+    python313Packages.isort
+    python313Packages.watchdog
   ];
 
   # Home Manager activation to install dclint globally via npm
-  home.activation.installDclint = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    export NPM_CONFIG_PREFIX="$HOME/.local"
+  home.activation.installDclint = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    export NPM_CONFIG_USERCONFIG=$HOME/.config/npm/npmrc
     command -v dclint >/dev/null || ${pkgs.nodejs}/bin/npm install -g dclint
   '';
 }
