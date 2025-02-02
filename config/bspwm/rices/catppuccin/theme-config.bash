9b4f2acc6e0b4416d240d30309cd2e148deff240
mshnwq <mshnwq.com@gmail.com>
2025-02-02 23:55:53 +0300

    working bar

--- config/bspwm/rices/catppuccin/theme-config.bash
+++ config/bspwm/rices/catppuccin/theme-config.bash
@@ -1,7 +1,3 @@
-#############################
-#		Daniela Theme		#
-#############################
-
 # (Catppuccin Mocha) colorscheme
 bg="#181825"
 fg="#CDD6F4"
@@ -11,6 +7,8 @@ red="#F38BA8"
 green="#A6E3A1"
 yellow="#F9E2AF"
 blue="#89B4FA"
+pink="#CBA6F7"
+purple="#BFA1FF"
 magenta="#F5C2E7"
 cyan="#94E2D5"
 white="#BAC2DE"
@@ -19,7 +17,6 @@ redb="#F38BA8"
 greenb="#A6E3A1"
 yellowb="#F9E2AF"
 blueb="#89B4FA"
-purple="#CBA6F7"
 magentab="#F5C2E7"
 cyanb="#94E2D5"
 whiteb="#A6ADC8"
@@ -31,6 +28,10 @@ tmuxw="true"
 BORDER_WIDTH="0"		# Bspwm border
 NORMAL_BC="#45475A"		# Normal border color
 FOCUSED_BC="#94E2D5"	# Focused border color
+bspc_left_padding="2"
+bspc_right_padding="2"
+# need match bar height + padding (55+2)
+bspc_bar_padding="57"
 
 # Terminal font & size
 term_opacity="0.80"
@@ -57,10 +58,13 @@ dunst_font='JetBrainsMono Nerd Font'
 dunst_border='0'
 
 # Gtk theme vars
-gtk_theme="CatppuccinMocha-zk"
+gtk_theme="Catppuccin-mocha-blue-standard+default"
+# gtk_theme="Yaru-magenta-dark"
 gtk_icons="Catppuccin-Mocha"
+# gtk_icons="Yaru-magenta-dark"
 gtk_cursor="Qogirr-Dark"
-geany_theme="z0mbi3-CatppuccinMocha"
+# gtk_cursor="Yaru-magenta-dark"
+# geany_theme="z0mbi3-CatppuccinMocha"
 
 # Wallpaper engine
 # Available engines:
