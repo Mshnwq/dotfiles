76d54678dc75310ab517682616637b1025301e2c
mshnwq <mshnwq.com@gmail.com>
2025-02-04 15:00:00 +0300

    update tmux

---
+++ home/.tmux/plugins/tmux-dracula/scripts/kitty.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+export LC_ALL=en_US.UTF-8
+
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $current_dir/utils.sh
+
+main() {
+  RATE=1
+
+  # Check if Kitty is running
+  if ! pgrep -x kitty >/dev/null; then
+    echo "No Kitty  "
+    exit 0
+  fi
+
+  KITTY_SOCKET="unix:/tmp/init-term-kitty.sock"
+
+  # If no socket is found, exit
+  if [[ -z "$KITTY_SOCKET" ]]; then
+    echo "No Kitty Socket  "
+    exit 0
+  fi
+
+  # Get the focused tab title using jq
+  KITTY_TAB=$(kitty @ --to "$KITTY_SOCKET" ls | jq -r '.[] | .tabs[] | select(.is_focused) | .title')
+
+  # Display result in Tmux status bar
+  if [[ -n "$KITTY_TAB" ]]; then
+    echo "${KITTY_TAB}  "
+  else
+    echo "No Active Tab  "
+  fi
+}
+
+main
