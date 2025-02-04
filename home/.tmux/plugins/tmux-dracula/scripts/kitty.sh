4378512c508b37c5327ca6580fcb791f2ccab8ec
mshnwq <mshnwq.com@gmail.com>
2025-02-04 15:00:00 +0300

    final kitty

--- home/.tmux/plugins/tmux-dracula/scripts/kitty.sh
+++ home/.tmux/plugins/tmux-dracula/scripts/kitty.sh
@@ -3,11 +3,7 @@
 #!/usr/bin/env bash
 export LC_ALL=en_US.UTF-8
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $current_dir/utils.sh
-
 main() {
-  RATE=1
 
   # Check if Kitty is running
   if ! pgrep -x kitty >/dev/null; then
@@ -15,6 +11,7 @@ main() {
     exit 0
   fi
 
+  RATE=1
   KITTY_SOCKET="unix:/tmp/init-term-kitty.sock"
 
   # If no socket is found, exit
