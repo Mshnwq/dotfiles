9c2774d6b8236a9c0c14c84bb03dca19fec6cd64
mshnwq <mshnwq.com@gmail.com>
2025-08-19 23:31:53 +0300

    add hyprl plugin build deps more

--- config/home-manager/lib/hyprWorkspaceLayouts.nix
+++ config/home-manager/lib/hyprWorkspaceLayouts.nix
@@ -1,19 +1,19 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, gnumake, gcc
-, hyprland, hyprgraphics, pixman, wayland, libdrm }:
-pkgs.stdenv.mkDerivation {
+{ lib, stdenv, fetchFromGitHub, pkg-config, gcc
+, hyprland, hyprgraphics, hyprutils, pixman, wayland, wlroots, libdrm, libinput }:
+stdenv.mkDerivation {
   pname = "hyprWorkspaceLayouts";
   version = "0.50.1";
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "zakk4223";
     repo = "hyprWorkspaceLayouts";
     rev = "760df97c3a9ba3991419a2885a4aa69503a599f1";
     hash = "sha256-aKsmvyhjlnHrVu4R25AJ4xB4B1mhgBL1Jdl106ItqHc=";
   };
-  nativeBuildInputs = [ pkg-config gnumake gcc ];
-  buildInputs = [ hyprland hyprgraphics pixman wayland libdrm ];
+  nativeBuildInputs = [ pkg-config gcc ];
+  buildInputs = [ hyprland hyprgraphics hyprutils pixman wayland wlroots libdrm libinput ];
   buildPhase = ''
     g++ -shared -Wall -fPIC --no-gnu-unique \
-      ./main.cpp ./workspaceLayout.cpp -g -std=c++23 \
+      ./main.cpp ./workspaceLayout.cpp -g -std=c++23 -DWLR_USE_UNSTABLE \
       `pkg-config --cflags pixman-1 libdrm wayland-server hyprland` \
       -o workspaceLayoutPlugin.so
   '';
