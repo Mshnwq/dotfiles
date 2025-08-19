61fe0f05fdbf518ac0eaa151e9e0060f8ce1acc0
mshnwq <mshnwq.com@gmail.com>
2025-08-19 08:52:14 +0300

    fix wall

--- setup/theme.sh
+++ setup/theme.sh
@@ -2,6 +2,6 @@ RICE="futhark"
 echo "$RICE" > ~/.config/dots/.rice
 
 WALL="$HOME/.config/dots/rices/$RICE/walls/glacier.png" 
-echo "$WALL" > ~/.config/dots/.rice
+echo "$WALL" > ~/.config/dots/.wall
 
 ~/.config/dots/scripts/WallColor $WALL
