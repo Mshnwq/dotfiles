4648b136e9fde2f71c63660205acf47b12f8749a
mshnwq <mshnwq.com@gmail.com>
2025-08-27 04:57:38 +0300

    uneeded setup script are just files now

--- setup/cpu.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/.build
-for i in {1..3}; do
-  git clone https://github.com/AdnanHodzic/auto-cpufreq.git ~/.build/auto-cpufreq && break
-  rm -rf ~/.build/auto-cpufreq
-  sleep 3
-done
-sudo ~/.build/auto-cpufreq/auto-cpufreq-installer --install
-sudo /usr/local/bin/auto-cpufreq --install
--- setup/flat.sh
+++
@@ -1,9 +0,0 @@
-# flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-modify --enable flathub
-
-flatpak install --system -y app/com.github.tchx84.Flatseal/x86_64/stable
-flatpak install --system -y app/org.mozilla.firefox/x86_64/stable
-flatpak install --system -y app/io.mpv.Mpv/x86_64/stable
-flatpak install --system -y app/io.github.getnf.embellish/x86_64/stable
-flatpak install --system -y app/io.podman_desktop.PodmanDesktop
-flatpak install --system -y app/io.github.dvlv.boxbuddyrs
