cf4380ec9ab870f8ed9f1c6c654f4b61bbe45296
mshnwq <mshnwq.com@gmail.com>
2025-11-04 21:00:49 +0300

    fix

--- config/dots/scripts/executer/.wal_obsidian.sh
+++ config/dots/scripts/executer/.wal_obsidian.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-xdg-open "obsidian://adv-uri?vault=Home&commandid=obsidian-pywal-theme%3Areload-pywal-theme"
+WINDOW_TITLE="Obsidian"
+
+if hyprctl clients -j | jq -e --arg title "$WINDOW_TITLE" '.[] | select(.title | contains($title))' >/dev/null; then
+    echo "refreshing obsidian"
+    xdg-open "obsidian://adv-uri?vault=Home&commandid=obsidian-pywal-theme%3Areload-pywal-theme"
+else
+    echo "not running obsidian"
+fi
