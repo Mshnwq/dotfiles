aa0e27af2c13e8573bc97db351598a3da0b22f03
mshnwq <mshnwq.com@gmail.com>
2025-11-07 23:21:04 +0300

    glim fork

--- config/home-manager/programs/rust.nix
+++ config/home-manager/programs/rust.nix
@@ -1,19 +1,29 @@
 # programs/rust.nix
 {
+  config,
   pkgs,
   ...
 }:
 let
   glim = pkgs.rustPlatform.buildRustPackage {
     pname = "glim";
-    version = "git-cd53dae";
+    # version = "git-cd53dae";
+    # src = pkgs.fetchFromGitHub {
+    #   owner = "junkdog";
+    #   repo = "glim";
+    #   rev = "cd53dae";
+    #   hash = "sha256-yAymON+o2slcyCpEq5prkffUelW5jV3I9JSJuQc6+jc=";
+    # };
+    # cargoHash = "sha256-9DxUgv10cSsTlwqTJWtNxcd/hbS6pGZ+XCPjL1wbCh8=";
+    version = "git-f141972";
     src = pkgs.fetchFromGitHub {
-      owner = "junkdog";
+      owner = "mshnwq";
       repo = "glim";
-      rev = "cd53dae";
-      hash = "sha256-yAymON+o2slcyCpEq5prkffUelW5jV3I9JSJuQc6+jc=";
+      rev = "f1419721699e400f9ca035cfd5b4fb72c58c6410";
+      hash = "sha256-vNJn2Xf8KBZMDD3hK0SXLQ9+84hDid2+NHNviU3oCGs=";
     };
     cargoHash = "sha256-9DxUgv10cSsTlwqTJWtNxcd/hbS6pGZ+XCPjL1wbCh8=";
+
     nativeBuildInputs = [ pkgs.pkg-config ]; # for build-time discovery
     buildInputs = [ pkgs.openssl ]; # OpenSSL headers & libs
   };
@@ -42,7 +52,9 @@ in
           let
             configFile = pkgs.writeText "glim.toml" ''
               gitlab_url = "https://gitlab.com/api/v4"
-              gitlab_token = "${config.sops.secrets."glim-token"}"
+              gitlab_token = ""
+              gitlab_token_file = "${config.sops.secrets."glim-token".path}"
+              animations = true
             '';
           in
           ''
