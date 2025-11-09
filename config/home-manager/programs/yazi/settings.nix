f7cf6a8be9990e7f81fc951182cf58cccbdc6959
mshnwq <mshnwq.com@gmail.com>
2025-11-10 02:39:04 +0300

    settings module

---
+++ config/home-manager/programs/yazi/settings.nix
@@ -0,0 +1,66 @@
+# programs/yazi/settings.nix
+{
+  ...
+}:
+{
+  mgr = {
+    show_hidden = false;
+    show_symlink = true;
+    scrolloff = 5;
+  };
+  preview = {
+    wrap = "no";
+    max_width = 800;
+    max_height = 1200;
+  };
+  opener = {
+    edit = [
+      {
+        run = "$EDITOR $@";
+        desc = "$EDITOR";
+        block = true;
+        for = "unix";
+      }
+    ];
+    open = [
+      {
+        run = "qimgv $1";
+        for = "unix";
+        when = ''extension in ["jpeg", "png", "jpg"]'';
+      }
+      {
+        run = "xdg-open $1";
+        desc = "Open";
+        for = "linux";
+      }
+    ];
+    reveal = [
+      {
+        run = ''xdg-open $(dirname "$1")'';
+        desc = "Reveal";
+        for = "linux";
+      }
+      {
+        run = ''exiftool "$1"; echo "Press enter to exit"; read _'';
+        block = true;
+        desc = "Show EXIF";
+        for = "unix";
+      }
+    ];
+    extract = [
+      {
+        run = "ya pub extract --list $@";
+        desc = "Extract here";
+        for = "unix";
+      }
+    ];
+    # WHY BROKEN?
+    play = [
+      {
+        run = ''xdg-open "$1"'';
+        desc = "Open";
+        for = "linux";
+      }
+    ];
+  };
+}
