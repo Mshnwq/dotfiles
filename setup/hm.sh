3037a46d68c7e66cffe905ac140bc6bae7dbbf96
mshnwq <mshnwq.com@gmail.com>
2025-11-16 00:42:15 +0300

    HM nice

--- setup/hm.sh
+++ setup/hm.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p "$HOME/.local/bin"
-cp -r "$HOME/.dotfiles/config/home-manager" "$HOME/.config/"
 export NIXPKGS_ALLOW_UNFREE=1
-cd ~/.config/home-manager && home-manager switch --impure "$@"
+export ENABLE_SOPS=true
+home-manager switch --impure "$@"
