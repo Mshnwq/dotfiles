3988bd55a48f04575952c82bcad00cc53f609a37
mshnwq <mshnwq.com@gmail.com>
2025-11-17 08:46:05 +0300

    not needed

--- setup/.rebase.sh
+++
@@ -1,8 +0,0 @@
-rpm-ostree rebase ostree-image-signed:docker://ghcr.io/mshnwq/mshnwq-atomic-solopasha:latest
-
-# rpm-ostree initramfs-etc --track=/etc/ostree/prepare-root.conf --reboot
-
-sudo bootc switch ghcr.io/mshnwq/bazzite-hyprland-nix:latest
-reboot
-sudo bootc switch --enforce-container-sigpolicy ghcr.io/mshnwq/bazzite-hyprland-nix:latest
-ujust install-system-flatpaks
