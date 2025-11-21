b0d7854b13b5bf43f1d3135d96d6fcfb9e7e7b3c
mshnwq <mshnwq.com@gmail.com>
2025-11-21 20:43:43 +0300

    add guitarpro

--- config/home-manager/packages/overlays.nix
+++ config/home-manager/packages/overlays.nix
@@ -1,4 +1,13 @@
 # packages/overlays.nix
-lib: final: prev: {
+{ lib, inputs }:
+final: prev: {
   shyfox = final.callPackage ./shyfox.nix { };
+  guitarpro = final.callPackage ./guitarpro.nix {
+    wine = final.wineWowPackages.stableFull;
+    makeDesktopItem = final.makeDesktopItem;
+    copyDesktopItems = final.copyDesktopItems;
+    makeDesktopIcon = inputs.erosanix.lib.${final.system}.makeDesktopIcon;
+    copyDesktopIcons = inputs.erosanix.lib.${final.system}.copyDesktopIcons;
+    mkWindowsAppNoCC = inputs.erosanix.lib.${final.system}.mkWindowsAppNoCC;
+  };
 }
