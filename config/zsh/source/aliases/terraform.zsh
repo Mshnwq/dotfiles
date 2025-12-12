548e0540b39feb647b6c14404d6cb97f3f9047d9
mshnwq <mshnwq.com@gmail.com>
2025-12-12 13:59:29 +0300

    better tf plan

--- config/zsh/source/aliases/terraform.zsh
+++ config/zsh/source/aliases/terraform.zsh
@@ -9,13 +9,41 @@ tf() {
   fi
 }
 
+tfpl() {
+  tf plan | awk '
+    {print; fflush()}
+    { 
+      line = $0
+      # to remove ANSI so `destroy` & `replaced`
+      gsub(/\x1b\[[0-9;]*m/, "", line)
+      if (line ~ /# .*will be created/) {
+        gsub(/.*# /, "", line);created[++c] = line
+      }
+      if (line ~ /# .*will be updated/) {
+        gsub(/.*# /, "", line); updated[++u] = line
+      }
+      if (line ~ /# .*(will be destroyed|must be replaced)/) {
+        gsub(/.*# /, "", line); destroyed[++d] = line
+      }
+    }
+    END {
+      if (c || u || d) {
+        print ""
+        if (c) { printf "\n✅ New resources:\n";        for (i=1; i<=c; i++) printf "  + %s\n", created[i] }
+        if (u) { printf "\n🔄 Resources to update:\n";  for (j=1; j<=u; j++) printf "  + %s\n", updated[j] }
+        if (d) { printf "\n❌ Resources to destroy:\n"; for (k=1; k<=d; k++) printf "  + %s\n", destroyed[k] }
+        print ""
+      }
+    }
+  '
+}
+
 alias tfin='tf init'
 alias tfap='tf apply'
 alias tfds='tf destroy'
 alias tfrf='tf refresh'
 alias tfvd='tf validate'
 alias tfmt='tf fmt'
-alias tfpl='tf plan'
+alias tfmv='tf mv'
 alias tfst='tf state'
 alias tfstls='tf state list'
-alias tfmv='tf mv'
