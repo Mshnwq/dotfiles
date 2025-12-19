bd925f480e3fe8944e3f2d80bd329b9bd294942f
mshnwq <mshnwq.com@gmail.com>
2025-12-19 10:48:59 +0300

    noce notf

--- local/bin/executer/.wal_dunst.sh
+++ local/bin/executer/.wal_dunst.sh
@@ -11,4 +11,6 @@ magick -size 64x64 xc:transparent -fill "${color4}" -font 'JetBrainsMono-NF-Extr
 magick -size 64x64 xc:transparent -fill "${color4}" -font 'JetBrainsMono-NF-ExtraBold' -pointsize 64 -draw 'gravity center text 0 0 "󰖀"' "$assets_dir/volume_medium.png"
 magick -size 64x64 xc:transparent -fill "${color4}" -font 'JetBrainsMono-NF-ExtraBold' -pointsize 64 -draw 'gravity center text 0 0 "󰕿"' "$assets_dir/volume_low.png"
 magick -size 64x64 xc:transparent -fill "${color4}" -font 'JetBrainsMono-NF-ExtraBold' -pointsize 64 -draw 'gravity center text 0 0 "󰝟"' "$assets_dir/volume_mute.png"
+sed -i "$assets_dir/locked.svg" -e "s/color:#.*/color:${color4}; }/"
+sed -i "$assets_dir/unlocked.svg" -e "s/color:#.*/color:${color4}; }/"
 sh -c "$HOME/.local/bin/executer/.dunst_start.sh"
