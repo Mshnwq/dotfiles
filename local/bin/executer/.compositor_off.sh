ff8898ead0b98a98939cf514bb214cc2bd5cdd66
mshnwq <mshnwq.com@gmail.com>
2026-01-08 13:45:53 +0300

    add compositor

--- local/bin/executer/.compositor_off.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source "$HOME/.config/kitty/cmds/opacity_off.sh"
-source "$HOME/.config/alacritty/cmds/opacity_off.sh"
-source "$HOME/.config/hypr/scripts/performance_on.sh"
--- local/bin/executer/.compositor_on.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source "$HOME/.config/kitty/cmds/opacity_on.sh"
-source "$HOME/.config/alacritty/cmds/opacity_on.sh"
-source "$HOME/.config/hypr/scripts/performance_off.sh"
