920b85ca16dd8924d6cd5f0b8325718e21c5d6bf
mshnwq <mshnwq.com@gmail.com>
2026-01-08 13:43:24 +0300

    add clean

--- local/bin/executer/.clean_on.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="$HOME/.local/bin/executer:$PATH"
-
-.compositor_off.sh
-.gesture_kill.sh
-.polkit_kill.sh
-.clean_k.sh
--- local/bin/executer/clean.sh
+++ local/bin/executer/clean.sh
@@ -1,2 +1,46 @@
-#WRAP_Toggle
-#k
+#!/usr/bin/env bash
+export PATH="$HOME/.local/bin/executer:$PATH"
+
+_procs() {
+  for app in "${@:2}"; do
+    procs "--$app" "$1"
+  done
+}
+
+apps=(
+  "gesture"
+  "ydotoold"
+)
+
+case $1 in
+--on)
+  _procs kill "${apps[@]}"
+  compositor --off
+  $0 --kde
+  ;;
+--off)
+  apps+=("dunst")
+  _procs start "${apps[@]}"
+  compositor --on
+  $0 --kde
+  ;;
+--status)
+  apps+=("dunst")
+  _procs status "${apps[@]}"
+  ;;
+--kde)
+  apps=(
+    "dolphin"
+    "kactivitymanagerd"
+    "kunifiedpush-distributor"
+    "kded6"
+    "kiod6"
+    "ksecretd"
+    "kwalletd"
+    "wsdd"
+  )
+  for app in "${apps[@]}"; do
+    pkill -f "$app"
+  done
+  ;;
+esac
