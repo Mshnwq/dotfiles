39496dd61ee0457bb058e1d50446d8852d121508
mshnwq <mshnwq.com@gmail.com>
2026-01-08 13:46:38 +0300

    add kde

--- local/bin/executer/.kde_disconnect.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo firewall-cmd --remove-port=1714-1764/udp --permanent
-sudo firewall-cmd --remove-port=1714-1764/tcp --permanent
-pkill kdeconnectd
-sudo firewall-cmd --reload
--- local/bin/executer/kde.sh
+++
@@ -1,3 +0,0 @@
-#WRAP_Action
-#connect
-#disconnect
