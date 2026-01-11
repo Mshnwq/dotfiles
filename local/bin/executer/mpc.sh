d426b59ab1cd2a74c01d69f3fb67a86ad1df1722
mshnwq <mshnwq.com@gmail.com>
2026-01-11 04:06:09 +0300

    add mpc

--- local/bin/executer/mpc.sh
+++ local/bin/executer/mpc.sh
@@ -1,3 +1,19 @@
-#WRAP_Action
-#local_refresh
-#remote_refresh
+#!/usr/bin/env bash
+
+LOCAL_DIR="$HOME/Music/"
+
+case $1 in
+--local-ref)
+  cd "$LOCAL_DIR" || exit 1
+  mpc clear && mpc update && sleep 2
+  find . -type f -name '*.m4a' | sed 's|^\./||' | while read -r song; do
+    mpc add "$song"
+  done
+  ;;
+--remote-ref)
+  REMOTE_DIR="/var/lib/mpd/music"
+  REMOTE_HOST_ALIAS="home-server"
+  rsync -avz --delete "$LOCAL_DIR" "${REMOTE_HOST_ALIAS}:$REMOTE_DIR"
+  ssh $REMOTE_HOST_ALIAS 'mpc_refresh.sh'
+  ;;
+esac
