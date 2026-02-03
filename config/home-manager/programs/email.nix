340aaead4cff34c1d89a834c567337c26a093164
mshnwq <mshnwq.com@gmail.com>
2026-02-03 16:15:42 +0300

    mkEmails

--- config/home-manager/programs/email.nix
+++ config/home-manager/programs/email.nix
@@ -5,93 +5,71 @@
   ...
 }:
 let
+  mkEmailAccount = name: secrets: {
+    realName = secrets.realName;
+    address = secrets.address;
+    userName = secrets.address;
+    primary = secrets.isPrimary;
+    signature = {
+      showSignature = "append";
+      text = secrets.realName;
+    };
+    imap = {
+      host = secrets.imap.host;
+      port = secrets.imap.port;
+    };
+    smtp = {
+      host = secrets.smtp.host;
+      port = secrets.smtp.port;
+    };
+    neomutt = {
+      enable = true;
+      extraConfig = ''
+        set edit_headers = yes
+        set charset = UTF-8
+        unset use_domain
+        set use_from = yes
+      '';
+    };
+    mbsync = {
+      enable = true;
+      create = "maildir";
+      patterns = [ "*" ];
+    };
+    passwordCommand = secrets.passwordCommand;
+  };
+
+  emailsPath = "${config.xdg.configHome}/emails.json";
   mailDir = "${config.home.homeDirectory}/Documents/Mail";
+  emailSecrets = builtins.fromJSON (builtins.readFile emailsPath);
+  emailAccounts = builtins.mapAttrs mkEmailAccount emailSecrets;
 in
 {
-  # https://home-manager.dev/manual/unstable/options.xhtml#opt-programs.neomutt.enable
   programs = {
     mbsync.enable = true;
+    # https://home-manager.dev/manual/unstable/options.xhtml#opt-programs.neomutt.enable
     neomutt = {
       enable = true;
       vimKeys = true;
       sort = "reverse-date";
       sidebar.enable = true;
     };
+    # https://home-manager.dev/manual/unstable/options.xhtml#opt-programs.aerc.enable
+    aerc = {
+      enable = true;
+    };
+  };
+
+  sops.secrets = {
+    emails = {
+      mode = "0400";
+      path = emailsPath;
+    };
   };
 
   # https://home-manager.dev/manual/unstable/options.xhtml#opt-accounts.email.accounts
   accounts.email = {
     maildirBasePath = mailDir;
-    accounts = {
-      "" = {
-        realName = "";
-        signature = {
-          showSignature = "append";
-          text = "";
-        };
-        address = "";
-        userName = "";
-        imap = {
-          host = "";
-          port = ;
-        };
-        smtp = {
-          host = "";
-          port = ;
-        };
-        neomutt = {
-          enable = true;
-          extraConfig = ''
-            set edit_headers = yes
-            set charset = UTF-8
-            unset use_domain
-            set use_from = yes
-          '';
-        };
-        mbsync = {
-          enable = true;
-          create = "maildir";
-          patterns = [ "*" ];
-        };
-        passwordCommand = "";
-      };
-      "" = {
-        realName = "";
-        signature = {
-          showSignature = "append";
-          text = "";
-        };
-        address = "";
-        userName = "";
-        imap = {
-          host = "";
-          port = ;
-        };
-        smtp = {
-          host = "";
-          port = ;
-        };
-        neomutt = {
-          enable = true;
-          extraConfig = ''
-            set edit_headers = yes
-            set charset = UTF-8
-            unset use_domain
-            set use_from = yes
-          '';
-        };
-        mbsync = {
-          enable = true;
-          create = "maildir";
-          patterns = [ "*" ];
-        };
-        passwordCommand = "";
-      };
-    };
+    accounts = emailAccounts;
   };
-
-  # https://home-manager.dev/manual/unstable/options.xhtml#opt-programs.aerc.enable
-  # programs.aerc = {
-  #   enable = true;
-  # };
 }
