61edccf3cca9a2862fb09a6925f034977997cc0e
mshnwq <mshnwq.com@gmail.com>
2026-02-06 17:37:15 +0300

    Spasiba

--- config/home-manager/programs/email.nix
+++ config/home-manager/programs/email.nix
@@ -70,14 +70,6 @@ in
           mouse-enabled = true;
         };
         filters = {
-          "text/plain" = "less -Rc";
-          # Filters allow you to pipe an email body through a shell command to render
-          # certain emails differently, e.g. highlighting them with ANSI escape codes.
-          # The first filter which matches the email's mimetype will be used, so order
-          # them from most to least specific.
-          # You can also match on non-mimetypes, by prefixing with the header to match
-          # against (non-case-sensitive) and a comma, e.g. subject,text will match a
-          # subject which contains "text". Use header,~regex to match against a regex.
           "subject,~^\\[PATCH" = "awk -f ${config.xdg.configHome}/aerc/filters/hldiff";
           "text/html" = "${config.xdg.configHome}/aerc/filters/html";
           "text/*" = "awk -f ${config.xdg.configHome}/aerc/filters/plaintext";
