31557803661e1e39781f9a82468eed5a1b380c8f
mshnwq <mshnwq.com@gmail.com>
2026-03-06 22:49:29 +0300

    init hotkeys

---
+++ config/home-manager/programs/obsidian/hotkeys.nix
@@ -0,0 +1,87 @@
+# programs/obsidian/hotkeys.nix
+{
+  ...
+}:
+{
+  "app:open-settings" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "`";
+    }
+  ];
+  "app:toggle-left-sidebar" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "E";
+    }
+  ];
+  "app:toggle-ribbon" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "R";
+    }
+  ];
+  "markdown:toggle-preview" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "Escape";
+    }
+  ];
+  "app:toggle-right-sidebar" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "\\";
+    }
+  ];
+  "workspace:split-vertical" = [
+    {
+      "modifiers" = [
+        "Mod"
+        "Shift"
+      ];
+      "key" = "\\";
+    }
+  ];
+  "window:zoom-in" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "=";
+    }
+  ];
+  "window:zoom-out" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = "-";
+    }
+  ];
+  "markdown:add-metadata-property" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = ",";
+    }
+  ];
+  "open-with-default-app:open" = [
+    {
+      "modifiers" = [
+        "Mod"
+      ];
+      "key" = ";";
+    }
+  ];
+}
