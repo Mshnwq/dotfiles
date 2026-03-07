1a1ae54f564c1a0f9dec570e13e5a6a95ad6f572
mshnwq <mshnwq.com@gmail.com>
2026-03-07 17:59:29 +0300

    upd new macros

--- config/home-manager/programs/news.nix
+++ config/home-manager/programs/news.nix
@@ -8,38 +8,41 @@ let
   theme = ''
     color info              color8 color0
     color background        color2 color0
-
     color listfocus         color8 color0 standout
     color listnormal        color3 color0
     color listfocus_unread  color8 color0 standout
     color listnormal_unread color4 color0
-
     highlight feedlist ".*0/0..---.*" color0 color0 invis
     highlight feedlist "---.*---"     color8 color0 standout
-
     search-highlight-colors    color6 color0
     searchresult-title-format "Search result"
-
     color article color7 default bold
-
     highlight article "^(Feed|Title|Author|Link|Date): .+" color6 color0 bold
     highlight article "^(Feed|Title|Author|Link|Date):"    color5 color0 bold
     highlight article "https?://[^ ]+"    color4 color0 underline
     highlight article "\\[[0-9][0-9]*\\]" color6 color0 bold
   '';
   binds = ''
-    macro b open-in-browser
-    macro c set browser "echo %u | wl-copy" ; open-in-browser ; set browser linkhandler
-    macro v set browser "sh -c 'nohup setsid Mpv %u --background=none --background-color=0/0 %u >/dev/null 2>&1 &'" ; open-in-browser ; set browser linkhandler
+    macro b set browser "Firefox %u" ; open-in-browser
+    macro m set browser "w3m %u" ; open-in-browser
+    macro c set browser "echo %u | wl-copy" ; open-in-browser
+    macro v set browser "sh -c 'nohup setsid Mpv %u --profile=transparent %u >/dev/null 2>&1 &'" ; open-in-browser
+    macro d set browser "yt-dlp -o '~/Downloads/News/%(title)s.%(ext)s' %u" ; open-in-browser
+    # macro y set browser "~/.local/bin/executer/.kitty-yt-img.sh %u" ; open-in-browser
     bind-key G end
     bind-key g home
-    bind-key k pageup
-    bind-key j pagedown
+    bind-key K pageup
+    bind-key J pagedown
     bind-key U show-urls
     bind-key n next-unread
     bind-key N prev-unread
     bind-key a toggle-article-read
   '';
+  # TODO: |
+  # https://github.com/newsboat/newsboat/blob/master/contrib/fltr-substack/README.md
+  # https://www.youtube.com/watch?v=XKOQb0f0OTg
+  # https://github.com/newsboat/newsboat/blob/master/contrib/image-preview/README.org
+  # https://github.com/Jocomol/newsboat_video_downloader
 in
 {
   sops.secrets = {
