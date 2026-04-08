ca87d30521a25de22120fa11dfef39d2d9e81989
mshnwq <mshnwq.com@gmail.com>
2026-04-09 00:04:11 +0300

    add anki to gtt

--- local/bin/executer/.gtt.sh
+++ local/bin/executer/.gtt.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -eo pipefail
 
-# TODO: cahnge to another file
 TITLE="TranTerm"
+ANKI_ADDRESS="localhost:8765"
+ANKI_AUDIO_DIR="$HOME/.local/share/Anki2/$USER/collection.media"
 DATA_DIR="$HOME/Documents/GTT"
 DST_FILE="$DATA_DIR/.dst"
 REPO_DIR="$DATA_DIR/repo"
@@ -11,7 +12,7 @@ REPO="https://github.com/eeeXun/gtt"
 [[ -s $DST_FILE ]] || echo "English" >"$DST_FILE"
 [[ -d $REPO_DIR ]] || git clone "$REPO" "$REPO_DIR"
 TRAN_FILE="$REPO_DIR/internal/translate/google/language.go"
-LANGS_FILE="$HOME/.config/keyboard_layouts.conf"
+LANGS_FILE="$HOME/.config/gtt_languages"
 
 goto_gtt() {
   local wid
@@ -31,7 +32,7 @@ extract_gtt() {
   sleep 0.1 && src=$(wl-paste)
   ydotool key 97:1 19:1 97:0 19:0
   sleep 0.1 && dst=$(wl-paste)
-  lang=$(grep -w "${1,,}" "$TRAN_FILE"| awk -F'"' '{print $2}')
+  lang=$(grep -w "$1" "$TRAN_FILE"| awk -F'"' '{print $2}')
   lang_dir="$DATA_DIR/$lang"
   [[ -d $lang_dir ]] || mkdir -p "$lang_dir"
   date_file="$lang_dir/$(date '+%Y-%m-%d').md"
@@ -49,16 +50,15 @@ select_gtt() {
       -markup-rows \
       -p "Translate Language" \
       -selected-row "$selected_index" \
-      -mesg "Current Language: $active_lang" \
+      -mesg "Current Language: ${active_lang^^}" \
       -theme "$HOME/.config/rofi/Selector.rasi" \
       -theme-str 'listview { columns: 2; lines: 2; }'
   }
   local langs
   declare -A langs
-  while read -r code lang; do
-    [[ $code == "US" ]] && continue # THE TODO: after
+  while IFS='=' read -r code lang; do
     [[ -n $lang && -n $code ]] && langs["$lang"]="$code"
-  done < <(cat "$LANGS_FILE" | cut -d '=' -f 1)
+  done <"$LANGS_FILE" 
   ((${#langs[@]} == 0)) && {
     echo "Error: No langs defined $LANGS_FILE"
     exit 1
@@ -84,8 +84,60 @@ select_gtt() {
 
 launch_gtt() {
   gtt -src "English" -dst \
-    "$(grep -w "${1,,}" "$TRAN_FILE" | awk -F'"' '{print $2}')"
+    "$(grep -w "$1" "$TRAN_FILE" | awk -F'"' '{print $2}')"
 }
+
+anki_gtt() {
+  local src dst date_file lang lang_dir audio
+  ydotool key 97:1 34:1 97:0 34:0
+  sleep 0.1 && src=$(wl-paste)
+  ydotool key 97:1 19:1 97:0 19:0
+  sleep 0.1 && dst=$(wl-paste)
+  audio="$ANKI_AUDIO_DIR/${dst}.mp3"
+  ydotool key 97:1 25:1 97:0 25:0
+  sleep 0.1
+  # https://www.reddit.com/r/archlinux/comments/x2kej0/recording_output_audio_using_pipewire/
+  pw-record -P '{ stream.capture.sink=true }' --format s16 --rate 44100 --channels 2 - | \
+    ffmpeg -f s16le -ar 44100 -ac 2 -i pipe:0 \
+      -af "silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,silenceremove=stop_periods=-1:stop_duration=0.5:stop_threshold=-50dB" \
+      -acodec mp3 "$audio" &
+  # local ffmpeg_pid=$!
+  # ( sleep 8; pkill -x pw-record ) &
+  # local safety_pid=$!
+  # wait $ffmpeg_pid
+  # kill $safety_pid 2>/dev/null
+  # pkill -x pw-record 2>/dev/null
+  # TODO: have it autodetect a silence and stop
+  # Monitor the audio levels and stop recording once silence over 2 secods is detected
+  # tee > stream to 2 ffmpegs 1 record, 1 detects silnce
+  sleep 8
+  pkill -x pw-record
+  sleep 1
+  # make a copy incase
+  lang=$(grep -w "$1" "$TRAN_FILE"| awk -F'"' '{print $2}')
+  lang_dir="$DATA_DIR/Anki/$lang"
+  [[ -d $lang_dir ]] || mkdir -p "$lang_dir"
+  cp "$audio" "$lang_dir/${src}|${dst}.mp3"
+  # https://github.com/FooSoft/anki-connect/issues/95
+  jq -n \
+    --arg src "$src" \
+    --arg dst "$dst" '{
+    action: "addNote",
+    version: 5,
+    params: {
+      note: {
+        deckName: "Learning",
+        modelName: "Basic",
+        fields: {
+          Front: $src,
+          Back: ($dst + " [sound:" + $dst + ".mp3]")
+        },
+        tags: ["gtt"]
+      }
+    }
+  }' | curl "$ANKI_ADDRESS" -X POST -d @- | jq
+}
+
 
 dst="$(<"$DST_FILE")"
 case $1 in
@@ -95,6 +147,14 @@ case $1 in
   if goto_gtt; then
     extract_gtt "$dst"
     dunstify "GTT: Extracted"
+  else
+    dunstify "GTT: Not Active"
+  fi
+  ;;
+--anki)
+  if goto_gtt; then
+    anki_gtt "$dst"
+    dunstify "GTT: Ankied"
   else
     dunstify "GTT: Not Active"
   fi
