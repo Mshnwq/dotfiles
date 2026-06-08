549aba422a41c112ab0d271c54ff0731a05f0cd2
mshnwq <hmachnouk@proton.me>
2026-06-08 03:26:43 +0300

    clean pass

--- config/home-manager/programs/pass.nix
+++ config/home-manager/programs/pass.nix
@@ -18,6 +18,7 @@
   home.packages = with pkgs; [
     pass
     pinentry-all
+    kdePackages.kleopatra
     (pkgs.symlinkJoin {
       name = "pinentry-rofi-wrapped";
       buildInputs = [ pkgs.makeWrapper ];
@@ -34,13 +35,6 @@
     PASSWORD_STORE_DIR = "${config.xdg.dataHome}/pass";
     GNUPGHOME = "${config.xdg.dataHome}/gnupg";
   };
-  # OpenSSH 		      ~/.ssh 	will not be fix -	Assumed to be present by many ssh daemons and clients such as DropBear and OpenSSH.
-  # SSH folder 		    ~/.ssh 			            700 	drwx------
-  # Public key 		    ~/.ssh/id_rsa.pub       644 	-rw-r--r--
-  # Private key 	    ~/.ssh/id_rsa 		      600 	-rw-------
-  # Authorized Keys 	~/.ssh/authorized_keys 	600 	-rw-------
-  # Config 		        ~/.ssh/config 		      600 	-rw-------
-
   # Ensure directory exists with secure permissions
   home.activation.ensureGnuPGDir =
     inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ]
@@ -64,31 +58,3 @@
     };
   };
 }
-
-# TODO: find way to sync all
-# for now manually do it
-# Source:
-#   (gpg)
-#     - gpg --export-secret-keys --armor KEY_ID > XXX-priv.asc
-#     - gpg --export             --armor KEY_ID > XXX-pub.asc
-#     - gpg --export-ownertrust > trust.txt
-#   (pass)
-#     - tar -czf backup.tar.gz -C ~/.pass .
-# Destination:
-#   (gpg)
-#     - gpg --import XXX-priv.asc
-#     - gpg --import XXX-pub.asc
-#     - gpg --import-ownertrust trust.txt
-#   (pass)
-#     - tar -xzf backup.tar.gz
-
-# TEST:
-# run ~/.dotfiles/setup/test/pass.sh
-
-# INFO:
-# Note that this currently does not work out-of-the-box using systemd user units and socket-based activation, since the socket directory changes based on the hash of $GNUPGHOME. You can get the new socket directory using gpgconf --list-dirs socketdir and have to modify the systemd user units to listen on the correct sockets accordingly. You also have to use the following gpg-agent.service drop-in file (or otherwise pass the GNUPGHOME env var to the agent running in systemd), or you might experience issues with "missing" private keys:
-#
-# [Service]
-# Environment="GNUPGHOME=%h/.local/share/gnupg"
-#
-# If you use GPG as your SSH agent, set SSH_AUTH_SOCK to the output of gpgconf --list-dirs agent-ssh-socket instead of some hardcoded value.
