f4d91272094af9169d2378cec5398d03e6cf34ca
mshnwq <hmachnouk@proton.me>
2026-07-20 23:39:29 +0300

    claude which

--- config/home-manager/programs/claude/default.nix
+++ config/home-manager/programs/claude/default.nix
@@ -1,10 +1,32 @@
+# programs/claude/default.nix
 {
   config,
+  pkgs,
   ...
 }:
 {
   programs.claude-code = {
     enable = true;
     configDir = "${config.xdg.configHome}/claude";
+    # settings = {
+    #   theme = "dark-ansi";
+    # };
+  };
+  home.packages = with pkgs; [
+    # TODO: find way to work with keyring (KWallet not gnome-keyring)
+    # claude-desktop # from /overlays
+    # claude-monitor # https://github.com/Maciek-roboblog/Claude-Code-Usage-Monitor
+    fff-mcp # from /pkgs
+    # outlook-mcp # from /pkgs
+    # openclaw
+  ];
+  programs.which-key = {
+    entries = [
+      {
+        key = "c";
+        desc = "Claude";
+        cmd = "claude-desktop";
+      }
+    ];
   };
 }
